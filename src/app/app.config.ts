2cf8d8f7929757b1e2be62e478406ed0a9d63380
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-12-01 19:16:39 +0100

    added toasts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(), 
+    provideToastr(),
   ]
 };
+
--- src/app/services/embauche-service.ts
+++ src/app/services/embauche-service.ts
@@ -1,6 +1,9 @@
 import { Injectable, signal } from '@angular/core';
 import { CVS } from '../data/cvs.data';
 import { Cv } from '../models/cv.model';
+import { ToastrService } from 'ngx-toastr';
+
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,15 +11,19 @@ export class EmbaucheService {
 
   embauches = signal<Cv[]>([]);
 
+  constructor(private toastr: ToastrService) {}
+
   embaucher(cv: Cv) {
     // éviter les doublons
     if (this.embauches().some((c) => c.id === cv.id)) {
-        alert("Cette personne est déjà embauchée");
+        //alert("Cette personne est déjà embauchée");
+        this.toastr.warning('Cette personne est déjà embauchée', 'Attention');
         return;
     }
 
     // ajouter un cv à embaucher 
     this.embauches.update(list => [...list, cv]);
+    this.toastr.success('Le CV a été embauché avec succès !', 'Succès');
 
   }
 
